b0ce43244a8ec5027d44379cba6ad30554e881fe
Vinícius Cavalcanti <vhpcavalcanti@outlook.com>
2022-06-19 14:34:23 -0300

    scaling widgets size

--- bim_visualizer_flutter/lib/presentation/pages/home.dart
+++ bim_visualizer_flutter/lib/presentation/pages/home.dart
@@ -7,6 +7,7 @@ import 'package:bim_visualizer_flutter/data/models/server_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:bim_visualizer_flutter/constants.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:another_flushbar/flushbar.dart';
 import 'package:dartssh2/dartssh2.dart';
 import 'package:flutter/material.dart';
 
@@ -47,37 +48,46 @@ class _HomeState extends State<Home> {
               ),
             ],
             child: Scaffold(
-              appBar: AppBar(
-                backgroundColor: secondaryColor,
-                title: const Text(
-                  'LG BIM Visualizer',
-                  style: TextStyle(color: primaryColor)
-                ),
-                elevation: 0,
-                actions: <Widget>[
-                  IconButton(
-                    padding: EdgeInsets.zero,
-                    icon: const Icon(Icons.refresh, color: primaryColor),
-                    onPressed: () {
-                      if (connected) client.close();
-                      _galaxyBloc.add(GalaxyConnect(server, 22));
-                    },
+              appBar: PreferredSize(
+                preferredSize: const Size.fromHeight(100.0),
+                child: AppBar(
+                  toolbarHeight: 100.0,
+                  backgroundColor: secondaryColor,
+                  title:  const Text(
+                    'LG BIM Visualizer',
+                    style: TextStyle(fontSize: 40, color: primaryColor)
                   ),
-                  IconButton(
-                    icon: const Icon(Icons.settings, color: primaryColor),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => Settings(
-                            preferencesBloc: _preferencesBloc,
-                            server: server
-                          )
-                        )
-                      );
-                    },
-                  ),
-                ],
+                  elevation: 0,
+                  actions: <Widget>[
+                    SizedBox(
+                      child: IconButton(
+                        padding: EdgeInsets.zero,
+                        icon: const Icon(Icons.refresh, size: 40.0, color: primaryColor),
+                        onPressed: () {
+                          if (connected) client.close();
+                          _galaxyBloc.add(GalaxyConnect(server, 22));
+                        },
+                      )
+                    ),
+                    SizedBox(
+                      width: 100.0,
+                      child: IconButton(
+                        icon: const Icon(Icons.settings, size: 40.0, color: primaryColor),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Settings(
+                                preferencesBloc: _preferencesBloc,
+                                server: server
+                              )
+                            )
+                          );
+                        },
+                      )
+                    ),
+                  ],
+                )
               ),
               body: Column(
                 children: <Widget>[
@@ -88,50 +98,78 @@ class _HomeState extends State<Home> {
                         client = state.client;
                       } else if (state is GalaxyConnectFailure) {
                         connected = false;
-                        ScaffoldMessenger.of(context).showSnackBar(
-                           SnackBar(
-                            content: Text(state.error),
-                            backgroundColor: errorColor,
-                          )
-                        );
+                        Flushbar(
+                          backgroundColor: errorColor,
+                          title: "Something goes wrong",
+                          isDismissible: true,
+                          flushbarStyle: FlushbarStyle.GROUNDED,
+                          titleSize: 26.0,
+                          message: state.error,
+                          messageSize: 22.0,
+                          duration: const Duration(seconds: 5),
+                          flushbarPosition: FlushbarPosition.TOP
+                        ).show(context);
                       }
                     },
                     builder: (blocContext, state) {
-                      return Card(
-                        color: secondaryColor,
-                        margin: EdgeInsets.zero,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(0)
-                        ),
-                        child: BlocConsumer<PreferencesBloc, PreferencesState>(
-                          listener: (context, state) {
-                            if (state is PreferencesUpdateSuccess || state is PreferencesClearSuccess) {
-                              _preferencesBloc.add(PreferencesGet());
-                            } else if (state is PreferencesGetSuccess) {
-                              if (connected) client.close();
-                              server = state.server;
-                              _galaxyBloc.add(GalaxyConnect(server, 22));
-                            }
-                          },
-                          builder: (context, state) {
-                            return ListTile(
-                              leading: Chip(
-                                label: Text(
-                                  connected ? 'Connected' : 'Disconnected',
-                                  style: const TextStyle(color: primaryColor)
-                                ),
-                                backgroundColor: connected ? successColor : errorColor
-                              ),
-                              title: Text(
-                                'Hostname: ' + server.hostname!,
-                                style: const TextStyle(color: primaryColor)
-                              ),
-                              subtitle: Text(
-                                'Address: ' + server.ipAddress!,
-                                style: const TextStyle(color: primaryColor)
-                              )
-                            );
-                          },
+                      return SizedBox(
+                        height: 100.0,
+                        child: Card(
+                          color: secondaryColor,
+                          margin: EdgeInsets.zero,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(0)
+                          ),
+                          child: BlocConsumer<PreferencesBloc, PreferencesState>(
+                            listener: (context, state) {
+                              if (state is PreferencesUpdateSuccess || state is PreferencesClearSuccess) {
+                                _preferencesBloc.add(PreferencesGet());
+                              } else if (state is PreferencesGetSuccess) {
+                                if (connected) client.close();
+                                server = state.server;
+                                _galaxyBloc.add(GalaxyConnect(server, 22));
+                              }
+                            },
+                            builder: (context, state) {
+                              return Row(
+                                children: <Widget>[
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 20.0),
+                                    child: Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Chip(
+                                        label: Text(
+                                          connected ? 'Connected' : 'Disconnected',
+                                          style: const TextStyle(fontSize: 22.0, color: primaryColor)
+                                        ),
+                                        backgroundColor: connected ? successColor : errorColor
+                                      )
+                                    )
+                                  ),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Padding(
+                                        padding: const EdgeInsets.only(left: 20.0),
+                                        child: Text(
+                                          'Hostname: ' + server.hostname!,
+                                          style: const TextStyle(fontSize: 24.0, color: primaryColor)
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.only(left: 20.0),
+                                        child: Text(
+                                          'Address: ' + server.ipAddress!,
+                                          style: const TextStyle(fontSize: 20.0, color: primaryColor)
+                                        )
+                                      ),
+                                    ]
+                                  )
+                                ]
+                              );
+                            },
+                          )
                         )
                       );
                     }
--- bim_visualizer_flutter/lib/presentation/pages/settings.dart
+++ bim_visualizer_flutter/lib/presentation/pages/settings.dart
@@ -19,9 +19,20 @@ class Settings extends StatelessWidget {
     return WillPopScope(
         onWillPop: () async => true,
         child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: secondaryColor,
-            title: const Text('Settings'),
+          appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(100.0),
+            child: AppBar(
+              leading: IconButton(
+                color: primaryColor,
+                icon: const Icon(Icons.arrow_back, size: 40.0),
+                onPressed: () => {
+                  Navigator.pop(context)
+                },
+              ),
+              toolbarHeight: 100.0,
+              backgroundColor: secondaryColor,
+              title: const Text('Settings', style: TextStyle(fontSize: 40)),
+            )
           ),
           body: SingleChildScrollView(
             child: Form(
@@ -36,72 +47,76 @@ class Settings extends StatelessWidget {
   Widget _buildForm(context, hostname, ipAddress, password) {
     return Padding(
       padding: const EdgeInsets.all(12.0),
-      child: Column(
-        children: <Widget>[
-          TextFormField(
-            initialValue: hostname,
-            decoration: const InputDecoration(
-              enabled: false,
-              labelText: 'hostname',
-              border: OutlineInputBorder()
-            )
-          ),
-          const SizedBox(height: 12),
-          TextFormField(
-            initialValue: server.ipAddress != '' ? server.ipAddress : '',
-            cursorColor: secondaryColor,
-            decoration: const InputDecoration(
-              labelText: 'IP Address',
-              border: OutlineInputBorder(),
-              labelStyle: TextStyle(color: secondaryColor),
-              focusedBorder: OutlineInputBorder(
-                borderSide:  BorderSide(color: secondaryColor),
-              ),
-            ),
-            onChanged: (value) => ipAddress = value,
-          ),
-          const SizedBox(height: 12),
-          TextFormField(
-            obscureText: true,
-            initialValue: server.password != '' ? server.password : '',
-            cursorColor: secondaryColor,
-            decoration: const InputDecoration(
-              labelText: 'Password',
-              border: OutlineInputBorder(),
-              labelStyle: TextStyle(color: secondaryColor),
-              focusedBorder: OutlineInputBorder(
-                borderSide:  BorderSide(color: secondaryColor),
-              ),
-            ),
-            onChanged: (value) => password = value,
-          ),
-          const SizedBox(height: 12),
-          ElevatedButton(
-            child: const Text('SAVE'),
-            style: ElevatedButton.styleFrom(
-              primary: accentColor,
-              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-              textStyle: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold
+      child: Padding(
+        padding: const EdgeInsets.all(45.0),
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              initialValue: hostname,
+              decoration: const InputDecoration(
+                contentPadding: EdgeInsets.all(30.0),
+                enabled: false,
+                labelText: 'Hostname',
+                labelStyle: TextStyle(fontSize: 30.0)
               )
             ),
-            onPressed: () {
-              // Instantiate a new server
-              Server server = Server(
-                hostname: hostname,
-                ipAddress: ipAddress,
-                password: password
-              );
+            const SizedBox(height: 12),
+            TextFormField(
+              initialValue: server.ipAddress != '' ? server.ipAddress : '',
+              cursorColor: secondaryColor,
+              decoration: const InputDecoration(
+                contentPadding: EdgeInsets.all(30.0),
+                labelText: 'IP Address',
+                labelStyle: TextStyle(fontSize: 30.0, color: secondaryColor),
+                focusedBorder: OutlineInputBorder(
+                  borderSide:  BorderSide(color: secondaryColor),
+                )
+              ),
+              onChanged: (value) => ipAddress = value,
+            ),
+            const SizedBox(height: 12),
+            TextFormField(
+              obscureText: true,
+              initialValue: server.password != '' ? server.password : '',
+              cursorColor: secondaryColor,
+              decoration: const InputDecoration(
+                contentPadding: EdgeInsets.all(30.0),
+                labelText: 'Password',
+                labelStyle: TextStyle(fontSize: 30.0, color: secondaryColor),
+                focusedBorder: OutlineInputBorder(
+                  borderSide:  BorderSide(color: secondaryColor),
+                )
+              ),
+              onChanged: (value) => password = value,
+            ),
+            const SizedBox(height: 12),
+            ElevatedButton(
+              child: const Text('SAVE'),
+              style: ElevatedButton.styleFrom(
+                primary: accentColor,
+                padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 20),
+                textStyle: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold
+                )
+              ),
+              onPressed: () {
+                // Instantiate a new server
+                Server server = Server(
+                  hostname: hostname,
+                  ipAddress: ipAddress,
+                  password: password
+                );
 
-              // emit preferences save state
-              preferencesBloc.add(PreferencesUpdate(server));
+                // emit preferences save state
+                preferencesBloc.add(PreferencesUpdate(server));
 
-              // navigate back
-              Navigator.pop(context);
-            },
-          )
-        ]
+                // navigate back
+                Navigator.pop(context);
+              },
+            )
+          ]
+        )
       )
     );
   }
