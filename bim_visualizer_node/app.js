cd264eb213c943f8eccc925fc964a6591c9f7bc3
Vinícius Cavalcanti <vhpcavalcanti@outlook.com>
2022-06-28 20:53:04 -0300

    fixed unity asset bundle compression type to work with webgl build

--- bim_visualizer_node/app.js
+++ bim_visualizer_node/app.js
@@ -4,22 +4,22 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'))
 
-app.use("/:screen/Build/client.framework.js.gz", (req, res, next) => {
-    res.contentType("application/x-javascript");
-    res.setHeader("Content-Encoding", "gzip");
-    res.sendFile(path.join(__dirname, "/public/client/Build/client.framework.js.gz"));
+app.use('/:screen/Build/client.framework.js.gz', (req, res, next) => {
+    res.contentType('application/x-javascript');
+    res.setHeader('Content-Encoding', 'gzip');
+    res.sendFile(path.join(__dirname, '/public/client/Build/client.framework.js.gz'));
 });
 
-app.use("/:screen/Build/client.data.gz", (req, res, next) => {
-    res.contentType("application/x-javascript");
-    res.setHeader("Content-Encoding", "gzip");
-    res.sendFile(path.join(__dirname, "/public/client/Build/client.data.gz"));
+app.use('/:screen/Build/client.data.gz', (req, res, next) => {
+    res.contentType('application/x-javascript');
+    res.setHeader('Content-Encoding', 'gzip');
+    res.sendFile(path.join(__dirname, '/public/client/Build/client.data.gz'));
 });
 
-app.use("/:screen/Build/client.wasm.gz", (req, res, next) => {
-    res.contentType("application/wasm");
-    res.setHeader("Content-Encoding", "gzip");
-    res.sendFile(path.join(__dirname, "/public/client/Build/client.wasm.gz"));
+app.use('/:screen/Build/client.wasm.gz', (req, res, next) => {
+    res.contentType('application/wasm');
+    res.setHeader('Content-Encoding', 'gzip');
+    res.sendFile(path.join(__dirname, '/public/client/Build/client.wasm.gz'));
 });
 
 app.use('/:screen', express.static(__dirname + '/public/client'))
