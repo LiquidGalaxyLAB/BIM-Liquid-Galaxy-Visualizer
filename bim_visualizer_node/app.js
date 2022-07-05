e841c7aba777b7705801f060e317684d53220737
Vinícius Cavalcanti <vhpcavalcanti@outlook.com>
2022-07-05 14:31:43 -0300

    emitting screen offset from server to unity

--- bim_visualizer_node/app.js
+++ bim_visualizer_node/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const { WebSocketServer } = require("ws");
+const wss = new WebSocketServer({ port: 3220 });
 
-app.use(express.static(__dirname + '/public'))
+const clients = [];
+
+app.use(express.static(__dirname + '/public'));
 
 app.use('/:screen/Build/client.framework.js.gz', (req, res, next) => {
     res.contentType('application/x-javascript');
@@ -22,9 +26,26 @@ app.use('/:screen/Build/client.wasm.gz', (req, res, next) => {
     res.sendFile(path.join(__dirname, '/public/client/Build/client.wasm.gz'));
 });
 
-app.use('/:screen', express.static(__dirname + '/public/client'))
-app.get('/:screen', (req, res) => {
-    res.sendFile('index.html');
+app.get('/:screen', (req, res, next) => {
+    let byteArr = [];
+    const buffer = Buffer.from(req.params.screen, "utf16le");
+    for (var i = 0; i < buffer.length; i++) {
+        byteArr.push(buffer[i]);
+    }
+    clients.push(byteArr);
+    next();
+});
+app.use('/:screen', express.static(__dirname + '/public/client'));
+
+wss.on("connection", (ws) => {
+    console.log("a user connected");
+
+    const encodedClientId = clients.shift();
+    ws.send(encodedClientId);
+
+    ws.on("close", () => {
+        console.log("client left");
+    });
 });
 
 module.exports = app;
--- bim_visualizer_node/server.js
+++ bim_visualizer_node/server.js
@@ -1,5 +1,4 @@
 const app = require('./app');
-const http = require("http").createServer(app);
 const PORT = 3210;
 
-http.listen(PORT, () => { console.log(`Server is listening on port ${PORT}`) });
+app.listen(PORT, () => { console.log(`Server is listening on port ${PORT}`) });
