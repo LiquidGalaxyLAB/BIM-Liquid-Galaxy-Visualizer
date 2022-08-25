75f6260be106cae9e0a4000e4066966b7f23a872
lg <vhpcavalcanti@outlook.com>
2022-08-25 08:53:47 -0300

    added pm2 to server install script

--- bim_visualizer_node/install.sh
+++ bim_visualizer_node/install.sh
@@ -2,6 +2,8 @@
 
 . ${HOME}/etc/shell.conf
 
+pass="$1"
+
 # Create logs directory if it doesnt exist yet
 mkdir -p ./logs
 
@@ -15,22 +17,20 @@ echo "[$time] Installing Galaxy BIM Visualizer..." | tee -a ./logs/$filename
 
 time=$(date +%H:%M:%S)
 echo "[$time] Installing new packages..." | tee -a ./logs/$filename
-sudo apt-get install -yq npm > ./logs/$filename
+echo $pass | sudo -S apt-get install -yq npm > ./logs/$filename
 
 time=$(date +%H:%M:%S)
 echo "[$time] Installing node..." | tee -a ./logs/$filename
-sudo npm install -g n > /dev/null 2>> ./logs/$filename
-sudo n stable > /dev/null
+echo $pass | sudo -S npm install -g n > /dev/null 2>> ./logs/$filename
+echo $pass | sudo -S n stable > /dev/null
 
 time=$(date +%H:%M:%S)
 echo "[$time] Installing local tunnel..." | tee -a ./logs/$filename
-sudo npm install -g localtunnel > /dev/null 2>> ./logs/$filename
+echo $pass | sudo -S npm install -g localtunnel > /dev/null 2>> ./logs/$filename
 
 time=$(date +%H:%M:%S)
 echo "[$time] Installing slave dependencies..." | tee -a ./logs/$filename
 for lg in $LG_FRAMES ; do
-  echo "Enter the $lg password"
-  read pass
   sshpass -p $pass scp public/logos.png $lg:/home/lg/
   sshpass -p $pass ssh lg@$lg "echo $pass | sudo -S apt-get install -yq feh"
   break
@@ -47,7 +47,7 @@ DATA=`cat /etc/iptables.conf | grep "tcp" | grep "8111" | grep "3210"`
 if [ "$DATA" == "" ]; then
   time=$(date +%H:%M:%S)
   echo "[$time] Port 3210 not open, opening port..." | tee -a ./logs/$filename
-  sudo sed -i "s/$LINE/$RESULT/g" /etc/iptables.conf 2>> ./logs/$filename
+  echo $pass | sudo -S sed -i "s/$LINE/$RESULT/g" /etc/iptables.conf 2>> ./logs/$filename
 else
   time=$(date +%H:%M:%S)
   echo "[$time] Port 3210 already open." | tee -a ./logs/$filename
@@ -64,7 +64,7 @@ DATA=`cat /etc/iptables.conf | grep "tcp" | grep "8111" | grep "3220"`
 if [ "$DATA" == "" ]; then
   time=$(date +%H:%M:%S)
   echo "[$time] Port 3220 not open, opening port..." | tee -a ./logs/$filename
-  sudo sed -i "s/$LINE/$RESULT/g" /etc/iptables.conf 2>> ./logs/$filename
+  echo $pass | sudo -S sed -i "s/$LINE/$RESULT/g" /etc/iptables.conf 2>> ./logs/$filename
 else
   time=$(date +%H:%M:%S)
   echo "[$time] Port 3220 already open." | tee -a ./logs/$filename
@@ -79,12 +79,12 @@ IS_ACTIVE=$(sudo systemctl is-active $SERVICE_NAME)
 if [ "$IS_ACTIVE" == "active" ]; then
   echo "Service $SERVICE_NAME is running" | tee -a ./logs/$filename
   echo "Restarting service" | tee -a ./logs/$filename
-  sudo systemctl restart $SERVICE_NAME
+  echo $pass | sudo -S systemctl restart $SERVICE_NAME
   echo "Service restarted" | tee -a ./logs/$filename
 else
   echo "Creating service $SERVICE_NAME"
 
-  sudo tee /etc/systemd/system/${SERVICE_NAME//'"'/}.service > /dev/null << EOM
+  echo $pass | sudo -S tee /etc/systemd/system/${SERVICE_NAME//'"'/}.service > /dev/null << EOM
 [Unit]
 Description=Localtunnel Daemon
 After=syslog.target network.target
@@ -106,9 +106,9 @@ EOM
 
   # restart daemon, enable and start service
   echo "Reloading daemon and enabling service" | tee -a ./logs/$filename
-  sudo systemctl daemon-reload 
-  sudo systemctl enable ${SERVICE_NAME//'.service'/} # remove the extension
-  sudo systemctl start ${SERVICE_NAME//'.service'/}
+  echo $pass | sudo -S systemctl daemon-reload 
+  echo $pass | sudo -S systemctl enable ${SERVICE_NAME//'.service'/} # remove the extension
+  echo $pass | sudo -S systemctl start ${SERVICE_NAME//'.service'/}
   echo "Service $SERVICE_NAME Started" | tee -a ./logs/$filename
 fi
 
@@ -119,12 +119,12 @@ IS_ACTIVE=$(sudo systemctl is-active $SERVICE_NAME)
 if [ "$IS_ACTIVE" == "active" ]; then
   echo "Service $SERVICE_NAME is running" | tee -a ./logs/$filename
   echo "Restarting service" | tee -a ./logs/$filename
-  sudo systemctl restart $SERVICE_NAME
+  echo $pass | sudo -S systemctl restart $SERVICE_NAME
   echo "Service restarted" | tee -a ./logs/$filename
 else
   echo "Creating service $SERVICE_NAME file"
 
-  sudo tee /etc/systemd/system/${SERVICE_NAME//'"'/}.service > /dev/null << EOM
+  echo $pass | sudo -S tee /etc/systemd/system/${SERVICE_NAME//'"'/}.service > /dev/null << EOM
 [Unit]
 Description=Localtunnel Daemon
 After=syslog.target network.target
@@ -146,23 +146,34 @@ EOM
 
   # restart daemon, enable and start service
   echo "Reloading daemon and enabling service" | tee -a ./logs/$filename
-  sudo systemctl daemon-reload 
-  sudo systemctl enable ${SERVICE_NAME//'.service'/} # remove the extension
-  sudo systemctl start ${SERVICE_NAME//'.service'/}
+  echo $pass | sudo -S systemctl daemon-reload 
+  echo $pass | sudo -S systemctl enable ${SERVICE_NAME//'.service'/} # remove the extension
+  echo $pass | sudo -S systemctl start ${SERVICE_NAME//'.service'/}
   echo "Service $SERVICE_NAME Started" | tee -a ./logs/$filename
 fi
 
-# Install dependencies
+# Install npm dependencies
 time=$(date +%H:%M:%S)
 echo "[$time] Installing server dependencies..." | tee -a ./logs/$filename
 npm install 2>> ./logs/$filename
 
+# Add access for pm2
+echo $pass | sudo -S chown lg:lg /home/lg/.pm2/rpc.sock /home/lg/.pm2/pub.sock
+
+# Stop server if already started
+pm2 delete BIMLGVIS_PORT:3210 2> /dev/null
+
+# Start server
+time=$(date +%H:%M:%S)
+echo "[$time] Starting pm2..." | tee -a ./logs/$filename
+pm2 start npm --name BIMLGVIS_PORT:3210 -- start 2>> ./logs/$filename
+pm2 save 2>> ./logs/$filename
+
+# Add automatic pm2 resurrect script
+time=$(date +%H:%M:%S)
+echo "[$time] Updating resurrect script..." | tee -a ./logs/$filename
+RESURRECT=$(pm2 startup | grep 'sudo')
+eval $RESURRECT 2>> ./logs/$filename
+
 time=$(date +%H:%M:%S)
 echo "[$time] Installation complete. Reboot machine to finish installation" | tee -a ./logs/$filename
-
-read -p "Do you want to reboot your machine now? [Y/n]: " yes
-
-if [[ $yes =~ ^[Yy]$ ]]
-then
-  sudo reboot
-fi
